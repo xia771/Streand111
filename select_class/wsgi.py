8bf137cdb88ca65f1ecc4068ad994a9a7e9ab984
runoob <test@runoob.com>
2024-12-18 17:13:21 +0800

    1

--- select_class/wsgi.py
+++ select_class/wsgi.py
@@ -9,10 +9,19 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import WSGIHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'select_class.settings')
 
-application = get_wsgi_application()
+class VercelWSGIHandler(WSGIHandler):
+    def __call__(self, environ, start_response):
+        try:
+            return super().__call__(environ, start_response)
+        except Exception as e:
+            import traceback
+            print('Error:', str(e))
+            print('Traceback:', traceback.format_exc())
+            raise
 
-# Vercel需要的app变量
+application = VercelWSGIHandler()
 app = application
---
+++ vercel_app.py
@@ -0,0 +1,4 @@
+from select_class.wsgi import application
+
+# Vercel needs the variable 'app'
+app = application
