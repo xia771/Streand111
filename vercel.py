f03ad513fea2a56283c00cda9c384f191a81449f
runoob <test@runoob.com>
2024-12-18 17:43:56 +0800

    Remove dj_database_url dependency and simplify configuration

--- vercel.py
+++ vercel.py
@@ -1,4 +1,9 @@
-from select_class.wsgi import application
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'select_class.settings')
+os.environ.setdefault('PYTHON_PATH', '.')
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 # Init the app
 app = application
